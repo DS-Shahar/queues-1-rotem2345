cce33fe557e917198983d130550e37253987704e
Rotem <127719291+rotem2345@users.noreply.github.com>
2024-03-03 16:51:46 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,100 @@
 class Main {
-  public static Queue<Wolter> buildQueue(){
-		Wolter w=new Wolter();
-		Queue<Wolter> q=new Queue<Wolter>();
-		for(int i=0;i<=10;i++) {
-			w=new Wolter();
-			q.insert(w);
-		}
-		q.remove();
-		return q;
-	}
-	
+	//_______________________________________________________________________ex18___________________________
+    public static Queue<Ball> fill(String[] a,int n){
+
+        boolean[] size= {true,false};
+        Queue<Ball> q=new Queue<Ball>();
+        int cnum;int bnum;
+        Ball b;
+        for (int i = 0; i < n; i++) {
+            cnum=(int)(Math.random() * ((5 )));
+            bnum=(int)(Math.random() * ((1)));
+
+            b=new Ball(a[cnum],size[bnum]);
+            q.insert(b);
+
+        }
+        return q;
+    }
+
+    public static void printQueue(Queue<Ball> q){
+        q.insert(null);
+        while(q.head()!=null){
+            System.out.print((char)q.head().getColor());
+            q.insert(q.remove());
+        }
+        q.remove();
+    }
+
+
+    public static int checkpos(Queue<Ball> q){
+        int count=1;
+        q.insert(null);
+        while((char)q.head().getColor()!='G' && q.head()!=null){
+            count++;
+            q.insert(q.remove());
+
+        }
+        if (q.head()==null){
+            return -1;
+        }
+        while (q.head()!=null){
+            q.insert(q.remove());
+        }
+        q.remove();
+        return count;
+
+    }
+
+    public static void playgame(Queue<Ball> q){
+        int pos= checkpos(q);
+        if (pos==-1){
+            System.out.println("eror, please reset game");
+            return;
+        }
+
+        System.out.print('\n'+ "enter a gues: ");
+
+        int gues=s.nextInt();
+        while(gues!=pos){
+            System.out.println("opss, not the right position");
+            printQueue(q);
+            System.out.println('\n'+"enter a gues: ");
+            gues=s.nextInt();
+
+        }
+        System.out.println('\n'+"nice! you found the golden ball");
+
+    }
+
+
+//______________________________________________________________ex20________________________________
+    public static void merge(Queue<Integer> q1,Queue<Integer> q2){
+        q1.insert(null);
+        q2.insert(null);
+        while(q1.head()!=null && q2.head()!=null){
+            if (q1.head()<=q2.head()){
+                q1.insert(q1.remove());
+            }
+            else{
+                q1.insert(q2.remove());
+            }
+        }
+        if(q1.head()==null){
+            while(q2.head()!=null) {
+                q1.insert(q2.remove());
+            }
+        }
+        else{
+            while(q2.head()!=null){
+                q1.insert(q1.remove());
+            }
+        }
+        q1.remove();
+        return;
+    }
+  
+//___________________________________________________________________ex21________________________	
 	public static String bla(Queue<Wolter> q,int cc) {
 		q.insert(null);
 		while(q.head().getCc()<cc && q.head()!=null) {
@@ -30,11 +115,4 @@ class Main {
 	}
 	
 
-	public static void main(String[] args) {
-		Queue<Wolter> q=buildQueue();
-		System.out.println(bla(q,70));
-		
-		
-
-	}
 }
